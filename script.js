9dd1dc58203e8cd2eceabcfb336f744b6bf8ed72
roadkillYeenXwX <igm0wuwfi@mozmail.com>
2022-09-14 09:48:02 +1000

    drop down menu

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+/* Assign event listener to the drop down menu button using immediately
+invoked function, hide drop down menu by clicking anywhere on the page except
+the button and the menu, to do so assign an even listener to the entire 
+document and use an if statement to check if an element is menu button/drop 
+down menu */
+
 (() => {
   const menuButton = document.getElementById('headerMenu');
   menuButton.addEventListener('click', function() {
