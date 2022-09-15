36706fd3ea2458580d2cdb0d2efa0dccc74d43f6
roadkillYeenXwX <igm0wuwfi@mozmail.com>
2022-09-15 17:21:02 +1000

    menu button for mobile browsers

--- script.js
+++ script.js
@@ -23,34 +23,52 @@ down menu */
   });
 })();
 
-/* rotate menu button on click and change background on mouseover*/
+/* rotate menu button on click and change background on mouseover,
+ check if mobile browser to assign different event listeners */
 
-(() => {
-  const menuButtonSvg = document.getElementById('headerMenu');
-  const menuButton = document.getElementById('svgContainerMenu');
-  const dropDownMenu = document.querySelector('.dropDownMenu');
-  document.addEventListener('click', function() {
-    const style = getComputedStyle(dropDownMenu)
-    const display = style.display;
-    if (display === 'none') {
-      menuButtonSvg.style.transform = 'rotate(0deg)'
-      menuButtonSvg.style.background = '#e5eaf1'
-    } else {
-      menuButtonSvg.style.transform = 'rotate(90deg)'
+
+if ("ontouchstart" in document.documentElement)
+{
+  (() => {
+    const menuButtonSvg = document.getElementById('headerMenu');
+    const dropDownMenu = document.querySelector('.dropDownMenu');
+    document.addEventListener('click', function() {
+      const style = getComputedStyle(dropDownMenu)
+      const display = style.display;
+      if (display === 'none') {
+        menuButtonSvg.style.transform = 'rotate(0deg)'
+        menuButtonSvg.style.background = '#e5eaf1'
+      } else {
+        menuButtonSvg.style.transform = 'rotate(90deg)'
+        menuButtonSvg.style.background = '#c8cad2'
+      }
+    });
+  })();
+} else {
+  (() => {
+    const menuButtonSvg = document.getElementById('headerMenu');
+    const menuButton = document.getElementById('svgContainerMenu');
+    const dropDownMenu = document.querySelector('.dropDownMenu');
+    document.addEventListener('click', function() {
+      const style = getComputedStyle(dropDownMenu)
+      const display = style.display;
+      if (display === 'none') {
+        menuButtonSvg.style.transform = 'rotate(0deg)'
+      } else {
+        menuButtonSvg.style.transform = 'rotate(90deg)'
+      }
+    });
+    menuButton.addEventListener('mouseover', function() {
       menuButtonSvg.style.background = '#c8cad2'
-    }
-  });
-  // menuButton.addEventListener('mouseover', function() {
-  //   menuButtonSvg.style.background = '#c8cad2'
-  // });
-  // menuButton.addEventListener('mouseout', function() {
-  //   menuButtonSvg.style.background = '#e5eaf1'
-  // });
-  menuButton.addEventListener('mousedown', function() {
-    menuButtonSvg.style.background = '#d6d8e1'
-  });
-  menuButton.addEventListener('mouseup', function() {
-    menuButtonSvg.style.background = '#c8cad2'
-  });
-})();
-
+    });
+    menuButton.addEventListener('mouseout', function() {
+      menuButtonSvg.style.background = '#e5eaf1'
+    });
+    menuButton.addEventListener('mousedown', function() {
+      menuButtonSvg.style.background = '#d6d8e1'
+    });
+    menuButton.addEventListener('mouseup', function() {
+      menuButtonSvg.style.background = '#c8cad2'
+    });
+  })();
+}
